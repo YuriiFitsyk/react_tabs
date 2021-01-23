7b242090aa2321ab82f64b3ddaec0a8034d68495
Yurii Fitsyk <yuriy.fitsyk@gmail.com>
2021-01-24 01:22:04 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.30c4e3e4fe102950a77b9f53174106e4.js"
 );
 
 self.addEventListener('message', (event) => {
